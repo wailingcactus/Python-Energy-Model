3816a63a622f1892b7fe84fca8eb2864055f93ea
wailingcactus <76265385+wailingcactus@users.noreply.github.com>
2022-09-27 17:00:46 +0200

    Enabling API call and graphing

--- API Test.py
+++ API Test.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests
 import json
 
@@ -6,15 +7,24 @@ def jprint(obj):
     print(text)
 
 parameters = {
-    "start": 20210101,
-    "end"  : 20210102,
+    "start": 20190101,
+    "end"  : 20191231,
     "latitude": 38.9573,
     "longitude": -75.4903,
     "community": "re",
-    "parameters": "ALLSKY_SFC_LW_DWN"
+    "parameters": "ALLSKY_SFC_SW_DWN",
+    "time-standard": "utc"
 }
 
 response = requests.get("https://power.larc.nasa.gov/api/temporal/hourly/point", params = parameters)
 
+efficiency = 0.2
+area = 10000
+
 print(response.status_code)
 jprint(response.json())
+
+r = response.json()
+irradiance_dictionary = r['properties']['parameter']['ALLSKY_SFC_SW_DWN']
+irradiance = pd.DataFrame(irradiance_dictionary.items())
+irradiance[2] = irradiance [1]*efficiency*area/1000 # Take the irradiance and calculate solar production based on efficiency and panel area in kWh
--- Demand.py
+++ Demand.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import requests
+import json
 import numpy as np
 
 demand = pd.read_excel('Delaware_Model_Data_2019.xlsx')
@@ -7,9 +9,40 @@ demand['monthyear']=pd.to_datetime(demand['datetime']).dt.strftime('%Y-%m')
 print(demand.to_string())
 
 
-demand.plot(x = 'Hours', y = ['Gas Usage [kWh]','Electricity usage [kWh]'])
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
 
-plt.title("Thermal Demand vs Hours for 2019")
+parameters = {
+    "start": 20190101,
+    "end"  : 20191231,
+    "latitude": 38.9573,
+    "longitude": -75.4903,
+    "community": "re",
+    "parameters": "ALLSKY_SFC_SW_DWN",
+    "time-standard": "utc"
+}
+
+response = requests.get("https://power.larc.nasa.gov/api/temporal/hourly/point", params = parameters)
+
+efficiency = 0.2
+area = 10000
+
+print(response.status_code)
+jprint(response.json())
+
+r = response.json()
+irradiance_dictionary = r['properties']['parameter']['ALLSKY_SFC_SW_DWN']
+irradiance = pd.DataFrame(irradiance_dictionary.items())
+irradiance[2] = irradiance [1]*efficiency*area/1000 # Take the irradiance and calculate solar production based on efficiency and panel area in kWh
+
+demand['Solar Production'] = irradiance[2]
+
+ax = demand.plot(x = 'Hours', y = ['Gas Usage [kWh]','Electricity usage [kWh]', 'Solar Production'])
+
+plt.title("Greenhouse Demand vs Hours for 2019")
 plt.xlabel("Hours")
-plt.ylabel("Thermal Demand (kWh)")
+plt.ylabel("Energy (kWh)")
 plt.show()
+
+
